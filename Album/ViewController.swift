735ce2e12a52c3ee4bcef93c095c5d001a7b105b
ckMoney <calvinlau717@gmail.com>
2021-01-28 19:17:17 +0800

    first commit

---
+++ Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Calvin Lau on 28/1/2021.
+//  Copyright © 2021 Calvin Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
